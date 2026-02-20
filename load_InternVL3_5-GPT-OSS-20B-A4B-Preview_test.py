2d02f623955256a7b9da87f795a11d271fc400eb
Tod Nestor <tmnestor@users.noreply.github.com>
2026-02-20 06:27:08 +0000

    test loading

---
+++ load_InternVL3_5-GPT-OSS-20B-A4B-Preview_test.py
@@ -0,0 +1,13 @@
+import torch
+from transformers import AutoTokenizer, AutoModel
+
+path = "/home/jovyan/nfs_share/models/InternVL3_5-GPT-OSS-20B-A4B-Preview"
+model = AutoModel.from_pretrained(
+    path,
+    torch_dtype=torch.bfloat16,
+    low_cpu_mem_usage=True,
+    use_flash_attn=True,
+    trust_remote_code=True,
+).eval().cuda()
+
+tokenizer = AutoTokenizer.from_pretrained(path, trust_remote_code=True, use_fast=False)
