a3c3c06aace46e23ea1cc429d28b1932136c937f
tmnestor <101078845+tmnestor@users.noreply.github.com>
2026-06-04 13:59:10 +1000

    a selective cp utility script

---
+++ scripts/cp_westpac.sh
@@ -0,0 +1,12 @@
+SRC=/Users/tod/Desktop/evaluation_data/synthetic_transaction_linking
+DST=${SRC}_westpac
+mkdir -p "$DST"
+
+for f in "$SRC"/*westpac*.png; do
+  case=${f##*/}; case=${case%%_*}          # CASE003_westpac_standard.png -> CASE003
+  /bin/cp -f "$SRC/${case}"_*.png "$DST/"  # copy every file for that case
+done
+
+Verify what landed:
+
+echo "files: $(/bin/ls "$DST" | wc -l) | westpac stmts: $(/bin/ls "$DST"/*westpac*.png | wc -l) | cases: $(/bin/ls "$DST"/*westpac*.png | sed -E 's#.*/(CASE[0-9]+)_.*#\1#' | sort -u | wc -l)"
